c57c2f484f31de857791efbab192e022ed4101b7
LoanNT <loannt@rikkeisoft.com>
2021-05-31 20:34:27 +0700

    add list seller

--- resources/views/new/seller-payment.blade.php
+++ resources/views/new/seller-payment.blade.php
@@ -10,28 +10,21 @@
             <div class="row">
                 Please contact to sellers in list, they will support you!
             </div>
+
             <div>
             <div class="row">
+                @foreach ($list_seller as $seller)
                 <div class="col s12 m6">
                     <div class="card blue-grey darken-1">
                         <div class="card-content white-text">
-                        <span class="card-title" style="font-style: italic">BOT</span>
-                        <p>Discord: B ⨶ T#3983</p>
-                        <p>Payment options such as: Paypal</p>
-                        <a href="https://discord.com/channels/762418378924359700/805535604015890452/805559942039863346">More Information</a>
+                        <span class="card-title" style="font-style: italic">{{$seller->seller_name}}</span>
+                        <p>Discord: {{ $seller->discord }}</p>
+                        <p>Payment options such as: {{ $seller->payment_options }}</p>
+                        <a href="{{$seller->more_infomation}}">More Information</a>
                         </div>
                     </div>
                 </div>
-                <div class="col s12 m6">
-                    <div class="card blue-grey darken-1">
-                        <div class="card-content white-text">
-                        <span class="card-title" style="font-style: italic">Razen</span>
-                        <p>Discord: Razen#5816</p>
-                        <p>Payment options such as: Paypal, Payoneer, TransferWise, Western Union, PaysafeCard ,Venmo, CashApp, Zelle, Apple Pay,...</p>
-                        <a href="https://discord.com/channels/762418378924359700/805535604015890452/805559942039863346">More Information</a>
-                        </div>
-                    </div>
-                </div>
+                @endforeach
             </div>
       </div>
     </form>
@@ -39,6 +32,6 @@
         </div>
     </div>
     <script !src="">
-        
+
     </script>
 @endauth
